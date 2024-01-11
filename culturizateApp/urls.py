0607cff0461daf9ae48106e1265cab27d1a6e7ad
Sally Galli <sergiogalli@MacBook-Air-de-Sally.local>
2024-01-11 19:11:39 +0100

    menu funcional principal

--- culturizateApp/urls.py
+++ culturizateApp/urls.py
@@ -6,17 +6,7 @@ from .views import obtain_categories
 
 
 
-"""
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("login/", CustomLoginView.as_view(), name='login'),
-    path("signin/",views.signin_view,name='signin'),
-   # path('login-with-template/', CustomLoginViewWithTemplate.as_view(), name='login_with_template'),
-    path('accounts/profile/',views.user_profile,name='profile'),
-    path('game/',views.obtener_pregunta_n1,name='game'),
-    #path('logout/', CustomLogoutView.as_view(), name='logout'),
-]
-"""
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", CustomLoginView.as_view(), name='login'),
